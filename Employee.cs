7e13362b65d82b93e230768cd0e19dcc1da49f48
Michelle Alzola <105762453+michellealzola@users.noreply.github.com>
2023-05-14 17:53:25 -0600

    Added Employee and ProductionWorker Classes

---
+++ Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workers_Data
+{
+    internal class Employee
+    {
+        public Employee(string name, int number)
+        {
+            Name = name;
+            Number = number;
+        }
+
+        public string Name { get; set; }
+        public int Number { get; set; }
+    }
+}
---
+++ ProductionWorker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workers_Data
+{
+    internal class ProductionWorker: Employee
+    {
+        public ProductionWorker(string name, int number, int shiftNumber, decimal hourlyPay) : base(name, number)
+        {
+            ShiftNumber = shiftNumber;
+            HourlyPay = hourlyPay;
+        }
+
+        public int ShiftNumber { get; set; }
+
+        public decimal HourlyPay { get; set; }
+    }
+}
