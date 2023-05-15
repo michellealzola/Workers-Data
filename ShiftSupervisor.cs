c7f72e8d61fc7762f6efbc887dfd51a7f58dce7c
Michelle Alzola <105762453+michellealzola@users.noreply.github.com>
2023-05-15 08:37:31 -0600

    Added ShiftSupervisor class

---
+++ ShiftSupervisor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workers_Data
+{
+    internal class ShiftSupervisor : Employee
+    {
+        public ShiftSupervisor(string name, int number, decimal annualSalary, decimal annualBonus) : base(name, number)
+        {
+            AnnualSalary = annualSalary;
+            AnnualBonus = annualBonus;
+        }
+
+        public decimal AnnualSalary { get; set; }
+        public decimal AnnualBonus { get; set; }
+    }
+}
