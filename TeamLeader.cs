174acd87ff738634446427a4c882731433a129e7
Michelle Alzola <105762453+michellealzola@users.noreply.github.com>
2023-05-15 10:04:35 -0600

    Added TeamLeader class

---
+++ TeamLeader.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workers_Data
+{
+    internal class TeamLeader : ProductionWorker
+    {
+        public TeamLeader(string name, int number, int shiftNumber, decimal hourlyPay, decimal monthlyBonus, int trainigHoursRequired, int trainingHoursActual ) : base(name, number, shiftNumber, hourlyPay)
+        {
+            MonthlyBonus = monthlyBonus;
+            TrainingHoursRequired = trainigHoursRequired;
+            TrainingHoursActual = trainingHoursActual;
+        }
+
+        public decimal MonthlyBonus { get; set; }
+
+        public decimal TrainingHoursRequired { get; set;}
+
+        public decimal TrainingHoursActual { get; set; }
+    }
+}
